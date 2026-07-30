71bebac14de3d30ecd162e0b3adad1e876e1f32e
SilentSpectre-arch <amirootrpr234@gmail.com>
2026-07-31 00:12:11 +0330

    feat:add check login shells

--- lib/users.sh
+++ lib/users.sh
@@ -31,3 +31,24 @@ check_empty_passwords() {
 		echo "$users_empty_pass"
 	fi
 }
+
+check_login_shells() {
+	local uid-min
+	local interactive_users
+	local interactive_user_count
+
+	uid_min=$(awk '/^UID_MIN/ {print $2}' /etc/login.defs)
+
+	interactive_users=$(awk -F: -v uid_min="$uid_min" '$3 !=0 && $3 < uid_min && ($7 == "/bin/bash" || $7 == "/bin/sh") {print $1}')
+	interactive_user_count=$(echo "$interactive_users" | sed '/^$/d' | wc -l)
+
+	if [[ "$interactive_user_count" -eq 0 ]];then
+		echo "[OK] No system users have interactive shells"
+	else
+		echo "[WARNING] Found $interactive_user_count system user(s) with interactive shells"
+		echo
+		echo "Users:"
+		echo "$interactive_users"
+	fi
+}
+
