45efff0bdfbc07c7da1e4f42017f7fb18c15c661
SilentSpectre-arch <amirootrpr234@gmail.com>
2026-07-30 20:37:02 +0330

    feat: add check permission

--- lib/permissions.sh
+++ lib/permissions.sh
@@ -1,7 +1,24 @@
 #!/usr/bin/env bash
 
 get_permission() {
-	local file="$1"
+	local file_name="$1"
 
 	stat -c "%a" "$file"
+}
+
+check_permission() {
+	local path_file="$1"
+	local expected_permission="$2"
+	local file_name="$3"
+
+	local current_permission
+	current_permission=$(get_permission "$path_file")
+
+	if [[ "$current_permission" == "$expected_permission" ]];then
+		echo "[OK] $file_name permission is correct ($current_permission)"
+	else
+		echo "[WARNING] $file_name permission is incorrect"
+		echo "Current: $current_permission"
+		echo "Expected: $expected_permission"
+	fi
 }
