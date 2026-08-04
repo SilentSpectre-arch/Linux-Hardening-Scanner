775b1e79745b56aca1bbda40ddaebb2e61c6897d
SilentSpectre-arch <amirootrpr234@gmail.com>
2026-08-04 23:36:27 +0330

    feat: add check sudoers.d permission

--- lib/sudo.sh
+++ lib/sudo.sh
@@ -17,3 +17,27 @@ check_sudoers_permission()
     fi
 }
 
+check_sudoers_d_permisson()
+{
+    local sudoers_d="/etc/sudoers.d"
+
+    if [ ! -d "$sudoers_d" ];then
+        echo "[INFO] No sudoers.d directory found"
+        return
+    fi
+
+    while read -r file;do
+        local current_permission
+
+        current_permission=$(stat -c "%a" "$file")
+
+        if [ "$current_permission" != "440" ];then
+            echo "[WARNING] $file permission is incorrect"
+            echo "Current: $current_permission"
+            echo "Expected: 440"
+        else
+            echo "[OK] $file permission is correct $current_permissoin"
+        fi
+    done < <(find "$sudoers_d" -type f)
+}
+
